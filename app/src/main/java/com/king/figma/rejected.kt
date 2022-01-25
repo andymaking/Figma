fe5df7a05b9cdd3ed98fe6f979fd86f38c853d19
andymaking <andymaking@gmail.com>
2022-01-25 13:56:06 +0100

    Initial commit

---
+++ app/src/main/java/com/king/figma/rejected.kt
@@ -0,0 +1,11 @@
+package com.king.figma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class rejected : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rejected)
+    }
+}
---
+++ app/src/main/java/com/king/figma/withdrawn.kt
@@ -0,0 +1,11 @@
+package com.king.figma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class withdrawn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_withdrawn)
+    }
+}
